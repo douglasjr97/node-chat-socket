04d47463b680b373211a3538568282b56e8eb417
douglasjr97 <falcao.douglas97@gmail.com>
2020-10-02 02:12:55 -0300

    first commit

---
+++ chat_client_tcp.js
@@ -0,0 +1,12 @@
+var socket = require('socket.io-client')('http://localhost:3000');
+const repl = require('repl');
+const chalk = require('chalk');
+socket.on('connect');
+socket.on('message');
+
+repl.start({
+    prompt:'',
+    eval: (mensagem) =>{
+        console.log(`Voce digitou ${mensagem}`)
+    }
+})
---
+++ chat_server_tcp.js
@@ -0,0 +1,18 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http)
+
+
+http.listen(3000, ()=>{console.log('Estou ouvindo')});
+
+
+io.on('connection', ()=>{
+    console.log('Cliente conectado', socket.id);
+
+    socket.on('message', (evt)=>{
+        console.log(evt);
+    })
+
+    socket.on('disconnect',()=>{
+        console.log('Cliente desconectado', socket.id);
+    })
+})
