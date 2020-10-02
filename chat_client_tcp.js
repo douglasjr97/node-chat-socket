abc60472121a5bf02cab4f49531ae7a6d54a4cf9
douglasjr97 <falcao.douglas97@gmail.com>
2020-10-02 16:03:56 -0300

    Funcional 1.0

--- chat_client_tcp.js
+++ chat_client_tcp.js
@@ -1,16 +1,31 @@
 var socket = require('socket.io-client')('http://localhost:3000');
-const repl = require('repl');
-//const chalk = require('chalk');
+const repl = require('repl')
+const chalk = require('chalk')
 
-//socket.on('connect');
-socket.on('message', (data) => {
-    console.log(data);
+let usuario = null;
+
+socket.on('connect',() => {
+    usuario = process.argv[2]
+    console.log(chalk.red(`Usuario ${usuario} acabou de entrar`))
 });
+
+
+
+
+socket.on('message', (data) => {
+    const {mensagem,usuario} = data;
+console.log(chalk.green(`${usuario} disse: ${mensagem}`))
+});
+
+
 
 repl.start({
     prompt:'',
     eval: (mensagem) => {
-       console.log(`Voce digitou ${mensagem}`)
-       socket.send(mensagem)
+       //console.log(`Voce digitou ${mensagem}`)
+       socket.send({
+        mensagem,
+        usuario
+       })
     }
 })
